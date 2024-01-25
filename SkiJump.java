bafc11c90112b5e34292cfdb1bb3c70dc31f5a18
Gigi <gli@loyola.edu>
2024-01-25 17:01:58 -0500

    wrote algoritm

---
+++ SkiJump.java
@@ -0,0 +1,19 @@
+// SkiJump.java
+// Programmer: Gigi Li
+// Course:  CS 212, Mr. John
+// Due Date: 2/1/24
+// In Class Assignment: 3
+// Problem Statement: Determine the score of the ski jumper using calculations
+// Data In: hill type, jumper's speed at the end of the ramp
+// Data Out: number of points earned
+// Credits: none
+
+import java.util.Scanner; //import scanner
+class SkiJump {
+    public static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in); //
+
+        
+    }
+}
